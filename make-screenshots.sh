7e5f6af90af4f73589f083c9c5a054eb598bbcd4
Kien Nguyen <kiennt2609@gmail.com>
2022-08-16 14:11:12 +0700

    Update script

--- make-screenshots.sh
+++ make-screenshots.sh
@@ -1,9 +1,43 @@
 #!/bin/bash
+
+usage() {
+    cat << HEREDOC
+    Usage:
+    $(basename $0) [-f] -i <filename>
+    -f: Force overwrite screenshots.
+    By default, <filename> is `colorschemes/**/*.json`, which means every files in colorschemes folder.
+
+HEREDOC
+    exit 0
+}
+
+FORCE=false
+FILES="colorschemes/**/*.json"
+
+for i in "$@"; do
+    case $i in
+        -f)
+            FORCE=true
+            shift
+            ;;
+        -h|--help)
+            usage
+            ;;
+        *)
+            FILES=$i
+            shift
+            ;;
+    esac
+done
+
 WINID=$(xdotool getmouselocation --shell | sed -n '/^WINDOW/s/.*=//p')
 
 shots=./screenshots
-rm -rf $shots
-for scheme in colorschemes/**/*.json ; do
+if [[ $FORCE = true ]]; then
+    rm -rf $shots
+fi
+
+for scheme in $FILES ; do
   clear
   prefix=$shots
   mkdir -p $prefix
